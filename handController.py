c1d40f804a9405e5d203dcee3866de23ab7016ff
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-01-02 12:47:32 +0530

    Hand controller is finally working

--- handController.py
+++ handController.py
@@ -5,11 +5,12 @@ mp_draw = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 prev_pos = 0
+tm = 0
 
 
 def Controller(q1):
 
-    global prev_pos
+    global prev_pos, tm
 
     cap = cv2.VideoCapture(1)
     with mp_hands.Hands(max_num_hands=1) as hand:
@@ -31,20 +32,27 @@ def Controller(q1):
                         frame, landmarks, mp_hands.HAND_CONNECTIONS)
 
                     open_fingers = finger_count(landmarks)
+                    print(open_fingers)
 
                     msg = ""
                     if open_fingers == 2:
                         msg = "Start"
-                    elif open_fingers == 0:
-                        msg = "Stop"
-                    elif open_fingers == 1:
-                        msg = "Up"
+                    # elif open_fingers == 0:
+                    #     msg = "Stop"
                     elif open_fingers == 5:
-                        msg = "Pause"
+                        if tm == 0:
+                            msg = "Up"
+                            tm = 30
+                    # elif open_fingers == 5:
+                    #     msg = "Pause"
 
                     q1.put(msg)
+                    if tm > 0:
+                        tm -= 1
 
-            cv2.line(frame, [0, h//2], [w, h//2], (0, 255, 0), 10)
+                    if tm == 0:
+                        cv2.line(frame, [0, h//2], [w, h//2], (0, 255, 0), 10)
+
             cv2.imshow("Controller window", cv2.flip(frame, 1))
 
             key = cv2.waitKey(1)
@@ -60,11 +68,11 @@ def finger_count(landmarks) -> int:
     i: int = 8
     n: int = len(landmarks.landmark)
 
-    if landmarks.landmark[4].y <= landmarks.landmark[5].y:
+    if landmarks.landmark[4].y >= landmarks.landmark[5].y:
         open_fingers += 1
 
     while (i < n):
-        if landmarks.landmark[i].y > landmarks.landmark[i-2].y:
+        if landmarks.landmark[i].y < landmarks.landmark[i-2].y:
             open_fingers += 1
         i += 4
     return open_fingers
